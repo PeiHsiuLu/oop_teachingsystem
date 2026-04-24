7d0e5d5df8ed79f1d6efb60e55cfc538e801408e
leokao960811 <leokao960811@gmail.com>
2026-04-24 22:00:36 +0800

    Updated Connection Methods

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-bcrypt = Bcrypt()
-login_manager = LoginManager()
+from mongoengine import connect
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///english_system.db'
-    app.config['SECRET_KEY'] = 'your-secret-key-here'
-
-    # Initialize extensions
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    Migrate(app, db)
-
-    # Import routes here to avoid circular imports
-    # from app.routes.auth import auth_bp
-    # app.register_blueprint(auth_bp)
-
+    app.config.from_object('config.Config')
+    
+    # Direct connection instead of using the wrapper
+    connect(host=app.config['MONGODB_SETTINGS']['host'])
+    
+    # ... rest of your code
     return app
--- config.py
+++ config.py
@@ -1,10 +1,6 @@
 import os
-from dotenv import load_dotenv
-
-# Load variables from a .env file if it exists
-load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-very-hard-to-guess-string'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///english_system.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MONGODB_SETTINGS = {
+        'host': os.environ.get('MONGO_URI', 'mongodb+srv://Three_People:Create_the_Miracle@oop.bzxogsx.mongodb.net/?appName=OOP')
+    }
